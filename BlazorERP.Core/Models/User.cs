923de2366495b57299297b39bf1adb30c0cd912b
Marvin Klein <support@marvinkleinmusic.de>
2024-03-26 03:59:24 +0100

    Projektdateien hinzufügen.

---
+++ BlazorERP.Core/Models/User.cs
@@ -0,0 +1,6 @@
+﻿using BlazorERP.Core.Interfaces;
+namespace BlazorERP.Core.Models;
+public class User : IDbModel
+{
+    public int Id { get; }
+}
---
+++ BlazorERP/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorERP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password, string? returnUrl = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
