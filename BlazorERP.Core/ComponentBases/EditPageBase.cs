af2d5144e9e7cdefa79ac1268e7a5916a90a82e1
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-04-10 15:48:58 +0200

    Add base EditPageBase

---
+++ BlazorERP.Core/ComponentBases/EditPageBase.cs
@@ -0,0 +1,35 @@
+﻿using BlazorERP.Core.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorERP.Core.ComponentBases;
+
+public abstract class EditPageBase<TIdentifier>
+{
+    protected EditForm? _form;
+
+    [Parameter]
+    public TIdentifier Identifier { get; set; }
+
+    /// <summary>
+    /// This method can be used to run more update logic during the transaction.
+    /// </summary>
+    /// <param name="dbController"></param>
+    /// <returns></returns>
+    protected virtual Task OnSaveAsync(IDbController dbController)
+    {
+        return Task.CompletedTask;
+    }
+
+    protected virtual Task BeforeSaveAsync(IDbController dbController)
+    {
+        return Task.CompletedTask;
+    }
+
+
+    protected virtual Task InitializeModelAsync(bool newEntry, IDbController dbController)
+    {
+        return Task.CompletedTask;
+    }
+
+}
