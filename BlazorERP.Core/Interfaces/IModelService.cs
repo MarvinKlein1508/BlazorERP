17948e36ea31c586289f8ed7655b6f5837a58f0d
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-04-10 16:46:17 +0200

    Add IModelService

---
+++ BlazorERP.Core/Interfaces/IModelService.cs
@@ -0,0 +1,88 @@
+﻿namespace BlazorERP.Core.Interfaces;
+
+/// <summary>
+/// Provides generalized CUD operations for an object Service.
+/// </summary>
+/// <typeparam name="TObject"></typeparam>
+public interface IModelService<TObject> : ICreateOperation<TObject>, IUpdateOperation<TObject>, IDeleteOperation<TObject>
+{
+
+}
+
+/// <summary>
+/// <para>
+/// Provides generalized CRUD operations for an object Service.
+/// </para>
+/// </summary>
+/// <typeparam name="TObject"></typeparam>
+/// <typeparam name="TIdentifier"></typeparam>
+public interface IModelService<TObject, TIdentifier> : IModelService<TObject>, IGetOperation<TObject, TIdentifier>
+{
+
+}
+
+
+/// <summary>
+/// Provides a generalized CREATE function for a specific type.
+/// </summary>
+/// <typeparam name="TObject"></typeparam>
+public interface ICreateOperation<TObject>
+{
+    /// <summary>
+    /// Saves the object as new entry in the database.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="dbController"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task CreateAsync(TObject input, IDbController dbController, CancellationToken cancellationToken = default);
+}
+
+/// <summary>
+/// Provides a generalized UPDATE function for a specific type.
+/// </summary>
+/// <typeparam name="TObject"></typeparam>
+public interface IUpdateOperation<TObject>
+{
+    /// <summary>
+    /// Updates an existing entry of the object in the database.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="dbController"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task UpdateAsync(TObject input, IDbController dbController, CancellationToken cancellationToken = default);
+}
+/// <summary>
+/// Provides a generalized DELETE function for a specific type.
+/// </summary>
+/// <typeparam name="TObject"></typeparam>
+public interface IDeleteOperation<TObject>
+{
+    /// <summary>
+    /// Deletes the object from the database.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="dbController"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task DeleteAsync(TObject input, IDbController dbController, CancellationToken cancellationToken = default);
+}
+/// <summary>
+/// Provides a generalized GET function for a specific type.
+/// </summary>
+/// <typeparam name="TObject"></typeparam>
+/// <typeparam name="TIdentifier"></typeparam>
+public interface IGetOperation<TObject, TIdentifier>
+{
+    /// <summary>
+    /// Gets the objects from the database
+    /// </summary>
+    /// <param name="identifier">The unique identifer for the object.</param>
+    /// <param name="dbController"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>
+    /// If the object does not exist than this method will return NULL.
+    /// </returns>
+    Task<TObject?> GetAsync(TIdentifier identifier, IDbController dbController, CancellationToken cancellationToken = default);
+}
--- BlazorERP.Core/Services/UserService.cs
+++ BlazorERP.Core/Services/UserService.cs
@@ -3,8 +3,18 @@ using BlazorERP.Core.Models;
 
 namespace BlazorERP.Core.Services;
 
-public class UserService
+public class UserService : IModelService<User, int>
 {
+    public Task CreateAsync(User input, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(User input, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
     public Task<User?> GetAsync(int userId, IDbController dbController, CancellationToken cancellationToken = default)
     {
         string sql = "SELECT * FROM USERS WHERE USER_ID = @USER_ID";
@@ -15,15 +25,21 @@ public class UserService
             USER_ID = userId
         }, cancellationToken);
     }
+    public Task UpdateAsync(User input, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+
     public async Task<User?> GetByUsernameAsync(string username, IDbController dbController, CancellationToken cancellationToken = default)
     {
         string sql = "SELECT * FROM USERS WHERE NORMALIZED_USERNAME = @USERNAME";
 
-        var result = await dbController.GetFirstAsync<User>(sql, 
+        var result = await dbController.GetFirstAsync<User>(sql,
         new
         {
             USERNAME = username.ToUpper()
-        },cancellationToken);
+        }, cancellationToken);
 
 
         return result;
