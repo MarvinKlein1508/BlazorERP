ca8bb67534e31a7faadbdc02c85798c4561d98bd
MarvinKlein1508 <support@marvinkleinmusic.de>
2024-04-10 15:41:14 +0200

    Add NormalizedUsername

--- BlazorERP.Core/Models/User.cs
+++ BlazorERP.Core/Models/User.cs
@@ -4,6 +4,7 @@ public class User : IDbModelWithName<int>
 {
     public int UserId { get; set; }
     public string Username { get; set; } = string.Empty;
+    public string NormalizedUsername => Username.ToUpper();
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
 
--- BlazorERP.Core/Services/UserService.cs
+++ BlazorERP.Core/Services/UserService.cs
@@ -7,13 +7,14 @@ public class UserService
 {
     public async Task<User?> GetByUsernameAsync(string username, IDbController dbController, CancellationToken cancellationToken = default)
     {
-        string sql = "SELECT * FROM USERS WHERE UPPER(USERNAME) = @USERNAME";
+        string sql = "SELECT * FROM USERS WHERE NORMALIZED_USERNAME = @USERNAME";
 
         var result = await dbController.GetFirstAsync<User>(sql, 
         new
         {
             USERNAME = username.ToUpper()
         },cancellationToken);
+
 
         return result;
     }
