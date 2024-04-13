e7d0f1deb9dd4c951a0b8f31bb1e5237a0c799db
Marvin Klein <support@marvinkleinmusic.de>
2024-04-13 17:18:27 +0200

    Add CustomDialogParameters

---
+++ src/BlazorERP.Core/ComponentBases/CustomDialogParameters.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace BlazorERP.Core.ComponentBases;
+
+public sealed class CustomDialogParameters : DialogParameters
+{
+    public string Text { get; set; } = string.Empty;
+}
