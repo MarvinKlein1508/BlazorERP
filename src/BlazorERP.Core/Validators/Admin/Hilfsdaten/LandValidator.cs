1185ce88314095998ecb1e670d731146394d479a
Marvin Klein <support@marvinkleinmusic.de>
2024-04-16 19:25:41 +0200

    Validators hinzugefügt

---
+++ src/BlazorERP.Core/Validators/Admin/Hilfsdaten/LandValidator.cs
@@ -0,0 +1,22 @@
+﻿using BlazorERP.Core.Models;
+using FluentValidation;
+
+namespace BlazorERP.Core.Validators.Admin.Hilfsdaten;
+
+public class LandValidator : AbstractValidator<Land>
+{
+    public LandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty();
+
+        RuleFor(x => x.Name)
+            .MaximumLength(30);
+
+        RuleFor(x => x.ISO2)
+            .Length(2);
+
+        RuleFor(x => x.ISO3)
+            .Length(3);
+    }
+}
---
+++ src/BlazorERP.Core/Validators/Admin/Hilfsdaten/WährungValidator.cs
@@ -0,0 +1,19 @@
+﻿using BlazorERP.Core.Models;
+using FluentValidation;
+
+namespace BlazorERP.Core.Validators.Admin.Hilfsdaten;
+
+public class WährungValidator : AbstractValidator<Währung>
+{
+    public WährungValidator()
+    {
+        RuleFor(x => x.Code)
+            .NotEmpty();
+
+        RuleFor(x => x.Code)
+            .MaximumLength(3);
+
+        RuleFor(x => x.Nachkommastellen)
+            .GreaterThanOrEqualTo(0);
+    }
+}
