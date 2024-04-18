f482ceb596c2444498a0d88a3a925ea0eb9997f8
Marvin Klein <support@marvinkleinmusic.de>
2024-04-18 19:45:38 +0200

    Validator hinzugefügt

---
+++ src/BlazorERP.Core/Validators/Stammdaten/KundeValidator.cs
@@ -0,0 +1,100 @@
+﻿using BlazorERP.Core.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace BlazorERP.Core.Validators.Stammdaten;
+
+public class KundeValidator : AbstractValidator<Kunde>
+{
+    public KundeValidator()
+    {
+        RuleFor(x => x.Firma)
+                .NotEmpty()
+                .When(x => string.IsNullOrWhiteSpace(x.Name1));
+
+        RuleFor(x => x.Firma)
+            .MaximumLength(60);
+
+        RuleFor(x => x.Name1)
+                .NotEmpty()
+                .When(x => string.IsNullOrWhiteSpace(x.Firma));
+
+        RuleFor(x => x.Name1)
+            .MaximumLength(60);
+
+        RuleFor(x => x.Name2)
+            .MaximumLength(60);
+
+        RuleFor(x => x.Strasse)
+            .MaximumLength(50)
+            .NotEmpty();
+
+        RuleFor(x => x.Ort)
+            .MaximumLength(40)
+            .NotEmpty();
+
+        RuleFor(x => x.LandId)
+            .NotNull()
+            .WithMessage("Wählen Sie ein Land aus.");
+
+        RuleFor(x => x.Postleitzahl)
+            .MaximumLength(8)
+            .NotEmpty();
+
+        RuleFor(x => x.SprachId)
+            .NotNull()
+            .WithMessage("Wählen Sie eine Sprache aus.");
+
+        RuleFor(x => x.Waehrungscode)
+            .NotNull()
+            .WithMessage("Wählen Sie eine Währung aus.");
+
+        RuleFor(x => x.AnredeId)
+            .NotNull()
+            .WithMessage("Wählen Sie eine Anrede aus.");
+
+        RuleFor(x => x.Telefonnummer)
+            .MaximumLength(30);
+
+        RuleFor(x => x.Mobilnummer)
+            .MaximumLength(30);
+
+        RuleFor(x => x.Faxnummer)
+            .MaximumLength(30);
+
+       
+
+        RuleFor(x => x.Email)
+            .MaximumLength(255)
+            .EmailAddress(FluentValidation.Validators.EmailValidationMode.AspNetCoreCompatible);
+
+        RuleFor(x => x.Website)
+            .MaximumLength(60);
+
+      
+
+        RuleFor(x => x.LieferbedingungId)
+            .NotNull()
+            .WithMessage("Wählen Sie eine Lieferbedingung aus.");
+
+        RuleFor(x => x.ZahlungsbedingungId)
+            .NotNull()
+            .WithMessage("Wählen Sie eine Zahlungsbedingung aus.");
+
+       
+        RuleFor(kund => kund.Kreditlimit)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(kund => kund.UmsatzsteuerIdentifikationsnummer)
+            .MaximumLength(30);
+
+        RuleFor(x => x.IBAN)
+            .MaximumLength(40);
+
+        RuleFor(x => x.BIC)
+            .MaximumLength(15);
+
+        RuleFor(x => x.Notiz)
+            .MaximumLength(1000);
+    }
+}
--- src/BlazorERP/Components/Pages/Stammdaten/Kunden/KundenForm.razor.cs
+++ src/BlazorERP/Components/Pages/Stammdaten/Kunden/KundenForm.razor.cs
@@ -2,50 +2,50 @@ using BlazorERP.Core.Enums;
 using BlazorERP.Core.Interfaces;
 using BlazorERP.Core.Models;
 using BlazorERP.Core.Validators.Admin.Hilfsdaten;
+using BlazorERP.Core.Validators.Stammdaten;
 using Microsoft.AspNetCore.Components;
 using System.Text.RegularExpressions;
 
-namespace BlazorERP.Components.Pages.Stammdaten.Kunden
+namespace BlazorERP.Components.Pages.Stammdaten.Kunden;
+
+public partial class KundenForm
 {
-    public partial class KundenForm
+
+    private bool _showOptionsMenu;
+    private KundeValidator _validator = new();
+
+    protected override string GetListUrl() => "/Stammdaten/Kunden";
+    protected override string GetEntityRedirectUrl() => $"/Stammdaten/Kunden/Edit?kundennummer={Input!.Kundennummer}";
+
+
+
+    [Parameter, SupplyParameterFromQuery(Name = "kundennummer")]
+    public override string? Identifier { get; set; }
+
+
+
+    protected override Task BeforeSaveAsync(IDbController dbController)
     {
-
-        private bool _showOptionsMenu;
-        private ZahlungsbedingungValidator _validator = new();
-
-        protected override string GetListUrl() => "/Stammdaten/Kunden";
-        protected override string GetEntityRedirectUrl() => $"/Stammdaten/Kunden/Edit?kundennummer={Input!.Kundennummer}";
-
-
-
-        [Parameter, SupplyParameterFromQuery(Name = "kundennummer")]
-        public override string? Identifier { get; set; }
-
-
-
-        protected override Task BeforeSaveAsync(IDbController dbController)
+        if (Modus is EditMode.Create)
         {
-            if (Modus is EditMode.Create)
-            {
-                Input.Anlagedatum = DateTime.Now;
-            }
-
-            Input.LetzterBearbeiter = User.UserId;
-            Input.ZuletztGeaendert = DateTime.Now;
-
-            return Task.CompletedTask;
+            Input.Anlagedatum = DateTime.Now;
         }
 
-        private void OnEmailChanged(string email)
-        {
-            Input.Email = email;
-            string domain = Regex.Replace(email, @".*@", "");
+        Input.LetzterBearbeiter = User.UserId;
+        Input.ZuletztGeaendert = DateTime.Now;
 
-            if (string.IsNullOrWhiteSpace(Input.Website))
-            {
-                Input.Website = domain;
-            }
-        }
-
+        return Task.CompletedTask;
     }
+
+    private void OnEmailChanged(string email)
+    {
+        Input.Email = email;
+        string domain = Regex.Replace(email, @".*@", "");
+
+        if (string.IsNullOrWhiteSpace(Input.Website))
+        {
+            Input.Website = domain;
+        }
+    }
+
 }
