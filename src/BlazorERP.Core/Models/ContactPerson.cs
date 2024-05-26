bc0b8169978f596717d9b8f837e940e6664d0023
Marvin Klein <support@marvinkleinmusic.de>
2024-05-26 21:28:00 +0200

    Add classes for ContactPersons

---
+++ src/BlazorERP.Core/Models/ContactPerson.cs
@@ -0,0 +1,30 @@
+﻿using BlazorERP.Core.Interfaces;
+
+namespace BlazorERP.Core.Models;
+
+public class ContactPerson : IDbModel<int?>
+{
+    public int ContactPersonId { get; set; }
+    public string Company { get; set; } = string.Empty;
+    public int? SalutationId { get; set; }
+    public Salutation? Salutation { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Department { get; set; } = string.Empty;
+    public int? LanguageId { get; set; }
+    public Language? Language { get; set; }
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string MobileNumber { get; set; } = string.Empty;
+    public string FaxNumber { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Note { get; set; } = string.Empty;
+    public int? LastModifiedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+
+    public int? GetIdentifier() => ContactPersonId > 0 ? ContactPersonId : null;
+
+    public Dictionary<string, object?> GetParameters()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/BlazorERP.Core/Services/ContactPersonService.cs
@@ -0,0 +1,48 @@
+﻿using BlazorERP.Core.Filters;
+using BlazorERP.Core.Interfaces;
+using BlazorERP.Core.Models;
+
+namespace BlazorERP.Core.Services;
+
+public class ContactPersonService : IModelService<ContactPerson, int?, ContactPersonFilter>
+{
+    public Task CreateAsync(ContactPerson input, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(ContactPerson input, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ContactPerson?> GetAsync(int? identifier, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ContactPerson>> GetAsync(int?[] identifiers, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ContactPerson>> GetAsync(ContactPersonFilter filter, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetFilterWhere(ContactPersonFilter filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> GetTotalAsync(ContactPersonFilter filter, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(ContactPerson input, IDbController dbController, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
