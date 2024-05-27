9fdc4d8a274dbce01ca3e3e83d9dfda0047eaedb
Marvin Klein <support@marvinkleinmusic.de>
2024-05-27 23:03:53 +0200

    Add AddressDialog

---
+++ src/BlazorERP.Core/Validators/MasterData/AddressValidator.cs
@@ -0,0 +1,12 @@
+﻿using BlazorERP.Core.Models;
+using FluentValidation;
+
+namespace BlazorERP.Core.Validators.MasterData;
+
+public class AddressValidator : AbstractValidator<Address>
+{
+    public AddressValidator()
+    {
+        
+    }
+}
