38ded9994c5b01723f01f6f553873d086982b399
Marvin Klein <support@marvinkleinmusic.de>
2024-06-19 19:15:14 +0200

    Add IApplicationMarker

---
+++ src/api/BlazorERP.Application/IApplicationMarker.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorERP.Application;
+
+/// <summary>
+/// This interface is being used for FluentValidation to register all validators from the containing assembly.
+/// </summary>
+internal interface IApplicationMarker
+{
+}
